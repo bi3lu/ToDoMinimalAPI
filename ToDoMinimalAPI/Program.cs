cb1bbedeb442e835eb6748d60648796a0cff2c44
Kuba Bielecki <kuba.bielecki@outlook.com>
2023-07-21 09:36:16 +0200

    Added necessary classes to project.
    Added class ToDo, ToDoService with IToDoService interface.
    Added endpoints for actions in Program.cs.

--- ToDoMinimalAPI/Program.cs
+++ ToDoMinimalAPI/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+using ToDoMinimalAPI.ToDo;
 
 namespace ToDoMinimalAPI
 {
@@ -8,7 +10,7 @@ namespace ToDoMinimalAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddAuthorization();
+            builder.Services.AddSingleton<IToDoService, ToDoService>();
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
@@ -25,27 +27,11 @@ namespace ToDoMinimalAPI
 
             app.UseHttpsRedirection();
 
-            app.UseAuthorization();
-
-            var summaries = new[]
-            {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    {
-                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                    .ToArray();
-                return forecast;
-            })
-            .WithName("GetWeatherForecast")
-            .WithOpenApi();
+            app.MapGet("/todos", (IToDoService service) => service.GetAll());
+            app.MapGet("/todos/{id}", (IToDoService service, Guid id) => service.GetById(id));
+            app.MapPost("/todos", (IToDoService service, ToDo.ToDo toDo) => service.Create(toDo));
+            app.MapPut("/todos/{id}", (IToDoService service, Guid id, ToDo.ToDo toDo) => service.Update(toDo));
+            app.MapDelete("/todos/{id}", (IToDoService service, Guid id) => service.Delete(id));
 
             app.Run();
         }
